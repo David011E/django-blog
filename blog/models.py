4d192f963db73060cbb7d0dc978e53e91e9c63aa
David011e <davidoliver011e@gmail.com>
2024-01-18 10:29:50 +0000

    Added Meta classes and STR methodfor Post and Comment Class

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,10 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"{self.title} | Written by {self.author}"
     
 
 class Comments(models.Model):
@@ -27,4 +31,8 @@ class Comments(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"{self.body} | by {self.author}"
  
