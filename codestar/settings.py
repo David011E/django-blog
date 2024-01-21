3b84fe16a6b7b312fb19b36141120512562f3c0d
David011e <davidoliver011e@gmail.com>
2024-01-21 12:00:12 +0000

    Added base template and homepage and syles

--- codestar/settings.py
+++ codestar/settings.py
@@ -20,6 +20,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
 # Quick-start development settings - unsuitable for production
@@ -63,7 +65,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
