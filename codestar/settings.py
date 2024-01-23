2bc85710c8dc3c0d60190a2bb5ace1f512f7aa44
David011e <davidoliver011e@gmail.com>
2024-01-23 16:19:20 +0000

    Ran some tests

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 import dj_database_url
+import sys
 if os.path.isfile('env.py'):
     import env
 
@@ -113,6 +114,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
 CSRF_TRUSTED_ORIGINS = [
